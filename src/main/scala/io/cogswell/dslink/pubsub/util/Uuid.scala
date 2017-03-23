ae43f8e2af4448bf70b8ddc9554681a197ed82f4
Joel Edwards <jedwards@aviatainc.com>
2017-03-23 16:18:41 -0600

    DGLOG-8
    src/main/scala/io/cogswell/dslink/pubsub/util/Futures.scala
    - new utility for converting common Java futures into Scala's Future
    
    src/main/scala/io/cogswell/dslink/pubsub/util/Uuid.scala
    - new utility for working with UUID

---
+++ src/main/scala/io/cogswell/dslink/pubsub/util/Uuid.scala
@@ -0,0 +1,12 @@
+package io.cogswell.dslink.pubsub.util
+
+import java.util.UUID
+
+object Uuid {
+  /**
+   * Creates a new UUID composed using the current time.
+   * 
+   * @return a new UUID
+   */
+  def now(): UUID = new UUID(System.currentTimeMillis(), System.nanoTime())
+}
