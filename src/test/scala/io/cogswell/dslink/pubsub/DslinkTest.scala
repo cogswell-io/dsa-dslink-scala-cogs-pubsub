3e92036805bd1304b93b66d53d171a27e1878f63
Joel Edwards <jedwards@aviatainc.com>
2017-03-24 17:15:36 -0600

    DGLOG-14
    src/main/scala/io/cogswell/dslink/pubsub/services/RunMode.scala
    src/main/scala/io/cogswell/dslink/pubsub/services/Services.scala
    src/main/scala/io/cogswell/dslink/pubsub/services/ServicesModule.scala
    - updates to support configuration of our dependency injection
    
    src/test/scala/io/cogswell/dslink/pubsub/DslinkTest.scala
    - base for all tests which correctly prepares our injection bindings
    
    src/test/scala/io/cogswell/dslink/pubsub/services/PubSubServiceTest.scala
    - the first test class, which serves as an example

---
+++ src/test/scala/io/cogswell/dslink/pubsub/DslinkTest.scala
@@ -0,0 +1,45 @@
+package io.cogswell.dslink.pubsub
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.{ global => globalEC }
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.Millis
+import org.scalatest.time.Nanoseconds
+import org.scalatest.time.Span
+
+import io.cogswell.dslink.pubsub.services.Services
+import io.cogswell.dslink.pubsub.services.ServicesModule
+import scaldi.Injector
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.Duration
+
+/**
+ * Base class for all DSLink tests. The patience is tunable via the
+ * timeout and interval parameters.
+ * 
+ * @param timeout the max duration to wait for a test to complete
+ * @param interval the tick frequency for checking whether a test should timeout
+ */
+abstract class DslinkTest(
+    timeout: Duration = Duration(10, TimeUnit.SECONDS),
+    interval: Duration = Duration(100, TimeUnit.MILLISECONDS)
+) extends FlatSpec with Matchers with ScalaFutures {
+  ServicesModule.setTestMode()
+
+  implicit val inj: Injector = Services.serviceInjector
+  implicit val ec: ExecutionContext = globalEC
+
+  /**
+   * Provides the default patience. If you wish to adjust the
+   * patience of your test, you can either override this function,
+   * or you can adjust the timeout and interval parameters in the
+   * constructor (preferred).
+   */
+  implicit def patience = PatienceConfig(
+      timeout = Span(timeout.toNanos, Nanoseconds),
+      interval = Span(interval.toNanos, Nanoseconds)
+   )
+}
---
+++ src/test/scala/io/cogswell/dslink/pubsub/services/PubSubServiceTest.scala
@@ -0,0 +1,49 @@
+package io.cogswell.dslink.pubsub.services
+
+import io.cogswell.dslink.pubsub.DslinkTest
+import scaldi.Injectable
+import io.cogswell.dslink.pubsub.connection.LocalPubSubConnection
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.Duration
+import scala.util.Try
+import scala.util.Success
+import scala.util.Failure
+import io.cogswell.dslink.pubsub.model.PubSubOptions
+
+/**
+ * This test serves primarily as an example for writing new DSLink tests.
+ * 
+ * General timeouts are tunable via the requisite parent class: DslinkTest.
+ * 
+ * This test confirms that injection is working correctly.
+ */
+class PubSubServiceTest extends DslinkTest(
+    timeout = Duration(5, TimeUnit.SECONDS),
+    interval = Duration(50, TimeUnit.MILLISECONDS)
+) with Injectable {
+  private def noKeys: Seq[String] = Seq.empty
+
+  "PubSubService" should "run in test mode within unit tests" in {
+    ServicesModule.runMode should be (TestMode)
+    
+    val pubsub: PubSubService = inject [PubSubService]
+    
+    pubsub should be (LocalPubSubService)
+  }
+  
+  it should "connect" in {
+    whenReady(Services.pubSubService.connect(noKeys, None)) { connection =>
+      connection should be (LocalPubSubConnection(None))
+    }
+
+    whenReady(Services.pubSubService.connect(noKeys, Some(PubSubOptions()))) { connection =>
+      connection should be (LocalPubSubConnection(Some(PubSubOptions())))
+    }
+
+    whenReady(
+        Services.pubSubService.connect(noKeys, None) flatMap { _.disconnect }
+    ) { result =>
+      result should be (Unit)
+    }
+  }
+}
