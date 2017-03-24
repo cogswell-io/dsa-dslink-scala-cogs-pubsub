8c8502546d6fbd00ae506e5bb8951769c7156dc3
Joel Edwards <jedwards@aviatainc.com>
2017-03-24 16:19:00 -0600

    DGLOG-14
    src/main/scala/io/cogswell/dslink/pubsub/util/LinkUtils.scala
    - completed the new action() function

--- src/main/scala/io/cogswell/dslink/pubsub/util/LinkUtils.scala
+++ src/main/scala/io/cogswell/dslink/pubsub/util/LinkUtils.scala
@@ -5,6 +5,48 @@ import org.slf4j.LoggerFactory
 import scala.util.Try
 import scala.util.Failure
 import scala.util.Success
+import org.dsa.iot.dslink.node.actions.Action
+import org.dsa.iot.dslink.node.actions.Action
+import org.dsa.iot.dslink.node.value.Value
+import org.dsa.iot.dslink.node.value.ValueType
+import org.dsa.iot.dslink.node.actions.ActionResult
+import org.dsa.iot.dslink.node.Permission
+import org.dsa.iot.dslink.node.actions.Parameter
+
+/**
+ * Class representing a parameter attached to an Action.
+ * 
+ * @param name the name of the parameter
+ * @param valueType the type of the value
+ * @param value the optional value
+ */
+case class ActionParam(
+    name: String,
+    valueType: ValueType,
+    value: Option[Value] = None
+) {
+  def parameter: Parameter = {
+    value match {
+      case None => new Parameter(name, valueType)
+      case Some(v) => new Parameter(name, valueType, v)
+    }
+  }
+}
+
+/**
+ * Class representing the data passed to an Action when
+ * it is invoked.
+ */
+case class ActionData(
+    params: Seq[ActionParam],
+    result: ActionResult
+) {
+  val dataMap: Map[String, ActionParam] = params.map {
+    case ActionParam(name, valueType, value) =>
+      val resultValue = Option(result.getParameter(name, valueType))
+      (name -> ActionParam(name, valueType, resultValue))
+  } toMap
+}
 
 object LinkUtils {
   /**
@@ -15,19 +57,39 @@ object LinkUtils {
    * 
    * @return the new Handler
    */
-  def handler[T](action: (T) => Unit): Handler[T] = new Handler {
+  def handler[T](action: (T) => Unit): Handler[T] = new Handler[T] {
     private val logger = LoggerFactory.getLogger(action.getClass)
     
     def handle(value: T): Unit = {
-      Try {
+      try {
         action(value)
-      } match {
-        case Success(_) =>
-        case Failure(error) => {
+      } catch {
+        case error: Throwable => {
           logger.error("Error in Handler:", error)
           throw error
         }
       }
     }
+  }
+  
+  /**
+   * Simplifies the creation of a new Action with parameters.
+   * 
+   * @param params the parameters which should be attached to this action
+   * @param permission 
+   */
+  def action(
+      params: Seq[ActionParam],
+      permission: Permission = Permission.READ
+  )(action: (ActionData) => Unit): Action = {
+    val actionHandler = handler { actionResult: ActionResult =>
+      action(ActionData(params, actionResult))
+    }
+    
+    val linkAction = new Action(permission, actionHandler)
+    
+    params.foreach(p => linkAction.addParameter(p.parameter))
+    
+    linkAction
   }
 }
