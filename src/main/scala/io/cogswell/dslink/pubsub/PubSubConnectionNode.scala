aff41be843ba196de47896b2e7731e8f5ce9b784
David Lewton <dlewton@aviatainc.com>
2017-03-24 18:01:39 -0600

    DGLOG-12: now attempting to create new node on 'invoke'

--- src/main/scala/io/cogswell/dslink/pubsub/PubSubConnectionNode.scala
+++ src/main/scala/io/cogswell/dslink/pubsub/PubSubConnectionNode.scala
@@ -5,6 +5,14 @@ import scala.concurrent.Future
 
 import org.dsa.iot.dslink.node.Node
 import org.dsa.iot.dslink.node.NodeManager
+import org.dsa.iot.dslink.node.value.ValueType
+import org.dsa.iot.dslink.node.actions.Action
+import org.dsa.iot.dslink.node.actions.Parameter
+import org.dsa.iot.dslink.node.actions.ActionResult
+import org.dsa.iot.dslink.node.Permission
+import org.dsa.iot.dslink.util.handler.Handler
+
+import org.slf4j.LoggerFactory
 
 import io.cogswell.dslink.pubsub.connection.PubSubConnection
 import io.cogswell.dslink.pubsub.services.Services
@@ -22,11 +30,29 @@ case class PubSubConnectionNode(
   private def options: Option[PubSubOptions] = url.map(u => PubSubOptions(url = u))
   private val keys: Seq[String] = Seq(readKey, writeKey, writeKey).filter(_.isDefined).map(_.get)
   private var connection: Option[PubSubConnection] = None
+  
+  private val logger = LoggerFactory.getLogger(getClass)
 
   private def initUi(): Unit = {
-    // TODO: setup UI
+    def handle(event: ActionResult) = {
+      logger.info(s"Clicked Invoke to Create a Do Something")
+    }
+    
+    val action = new Action(Permission.WRITE, new Handler[ActionResult] { def handle(event: ActionResult) {}})
+    /*  .addParameter(new Parameter(HOST_PARAMETER, ValueType.STRING, new Value("default_host")))
+      .addParameter(new Parameter(READKEY_PARAMETER, ValueType.STRING))
+      .addParameter(new Parameter(WRITEKEY_PARAMETER, ValueType.STRING))
+      .addParameter(new Parameter(ADMINKEY_PARAMETER, ValueType.STRING))
+      .addParameter(new Parameter(NAME_PARAMETER, ValueType.STRING))*/
+    
+    val connectNode = parentNode
+      .createChild(name)
+      .setDisplayName(name)
+      .setAction(action)
+      .build()
+
   }
-  
+
   initUi()
   
   def connect()(implicit ec: ExecutionContext): Future[PubSubConnection] = {
--- src/main/scala/io/cogswell/dslink/pubsub/PubSubRootNode.scala
+++ src/main/scala/io/cogswell/dslink/pubsub/PubSubRootNode.scala
@@ -2,7 +2,7 @@ package io.cogswell.dslink.pubsub
 
 import scala.collection.mutable.MutableList
 import org.dsa.iot.dslink.DSLink
-import org.dsa.iot.dslink.util.handler.Handler
+import org.dsa.iot.dslink.node.value.Value
 import org.dsa.iot.dslink.node.value.ValueType
 import org.dsa.iot.dslink.node.actions.Action
 import org.dsa.iot.dslink.node.actions.Parameter
@@ -10,6 +10,9 @@ import org.dsa.iot.dslink.node.actions.ActionResult
 import org.dsa.iot.dslink.node.Permission
 import org.dsa.iot.dslink.node.NodeManager
 import org.dsa.iot.dslink.node.Node
+import org.dsa.iot.dslink.util.handler.Handler
+
+import org.slf4j.LoggerFactory
 
 case class PubSubRootNode(
     link: DSLink
@@ -18,22 +21,45 @@ case class PubSubRootNode(
   private lazy val rootNode: Node = manager.getNode("/").getNode
 
   private val connections: MutableList[PubSubConnectionNode] = new MutableList
+  private val logger = LoggerFactory.getLogger(getClass)
 
   private def initUi(): Unit = {
-    // logger.info(s"Responder for path '${link.getPath}' has been initialized")
-    val CHILD_NAME = "Connect"
-    val CHILD_TITLE = "Connect"
+    val HOST_PARAMETER = "host"
+    val READKEY_PARAMETER = "read"
+    val WRITEKEY_PARAMETER = "write"
+    val ADMINKEY_PARAMETER = "admin"
+    val NAME_PARAMETER = "name"
     
-    val action = new Action(Permission.WRITE, new Handler[ActionResult] { def handle(event: ActionResult) = {}})
-      .addParameter(new Parameter("host", ValueType.STRING))
-      .addParameter(new Parameter("read", ValueType.STRING))
-      .addParameter(new Parameter("write", ValueType.STRING))
-      .addParameter(new Parameter("admin", ValueType.STRING))
-      .addParameter(new Parameter("name", ValueType.STRING))
+    val addConnectionHandler = new Handler[ActionResult] { 
+      def handle(event: ActionResult) = {
+        logger.info(s"Clicked Invoke to Create a Connection")
+        logger.info(s"Parameter Values - '${HOST_PARAMETER}': ${event.getParameter(HOST_PARAMETER).getString()}")
+        logger.info(s"Parameter Values - '${HOST_PARAMETER}': ${event.getParameter(READKEY_PARAMETER)}")
+        
+        val blah : Value = event.getParameter(READKEY_PARAMETER)
+        
+        addConnection(
+          Option(event.getParameter(NAME_PARAMETER)).getOrElse(new Value("default_name")).getString(),
+          Option(Option(event.getParameter(NAME_PARAMETER)).getOrElse(new Value("read_key")).getString()),
+          Option(Option(event.getParameter(NAME_PARAMETER)).getOrElse(new Value("write_key")).getString()),
+          Option(Option(event.getParameter(NAME_PARAMETER)).getOrElse(new Value("admin_key")).getString()),
+          Option(event.getParameter(NAME_PARAMETER)).getOrElse(new Value("default_url")).getString()
+        )
+        
+        logger.info("Connection node should now exist")
+      } 
+     }
     
-    val builder = rootNode
-      .createChild(CHILD_NAME)
-      .setDisplayName(CHILD_TITLE)
+    val action = new Action(Permission.WRITE, addConnectionHandler)
+      .addParameter(new Parameter(HOST_PARAMETER, ValueType.STRING, new Value("default_host")))
+      .addParameter(new Parameter(READKEY_PARAMETER, ValueType.STRING))
+      .addParameter(new Parameter(WRITEKEY_PARAMETER, ValueType.STRING))
+      .addParameter(new Parameter(ADMINKEY_PARAMETER, ValueType.STRING))
+      .addParameter(new Parameter(NAME_PARAMETER, ValueType.STRING))
+    
+    rootNode
+      .createChild("connection")
+      .setDisplayName("Connect")
       .setAction(action)
       .build()
   }
