07daa8f819e0d1dd6c02ba6affa86f705cebe4cb
Joel Edwards <jedwards@aviatainc.com>
2017-03-28 17:28:32 -0600

    DGLOG-32
    src/main/scala/io/cogswell/dslink/pubsub/util/CogsUtils.scala
    - new utility for working with the Cogswell Java SDK
    
    src/main/scala/io/cogswell/dslink/pubsub/model/PubSubOptions.scala
    - added translaters from listeners (functions) to handlers (Java SDK interfaces)
    - added listeners matching the handlers defined on the pub/sub handle
    
    src/main/scala/io/cogswell/dslink/pubsub/PubSubConnectionNode.scala
    - added the status node
    - updating status node on close and reconnect
    
    src/main/scala/io/cogswell/dslink/pubsub/model/PubSubMessage.scala
    - added a convenience method to build from an SDK native message record
    
    src/main/scala/io/cogswell/dslink/pubsub/services/CogsPubSubService.scala
    - now adding all listeners in options as handlers on the handle

--- src/main/scala/io/cogswell/dslink/pubsub/model/PubSubOptions.scala
+++ src/main/scala/io/cogswell/dslink/pubsub/model/PubSubOptions.scala
@@ -1,5 +1,15 @@
 package io.cogswell.dslink.pubsub.model
 
+import com.gambit.sdk.pubsub.responses.errors.PubSubErrorResponse
+import com.gambit.sdk.pubsub.handlers.PubSubRawRecordHandler
+import com.gambit.sdk.pubsub.handlers.PubSubMessageHandler
+import com.gambit.sdk.pubsub.handlers.PubSubReconnectHandler
+import com.gambit.sdk.pubsub.handlers.PubSubErrorHandler
+import io.cogswell.dslink.pubsub.util.CogsUtils
+import com.gambit.sdk.pubsub.PubSubMessageRecord
+import com.gambit.sdk.pubsub.handlers.PubSubCloseHandler
+import com.gambit.sdk.pubsub.handlers.PubSubErrorResponseHandler
+
 /**
  * Options for a pub/sub connection.
  * 
@@ -10,8 +20,38 @@ package io.cogswell.dslink.pubsub.model
 case class PubSubOptions(
     messageListener: Option[(PubSubMessage) => Unit] = None,
     errorListener: Option[(Throwable) => Unit] = None,
+    closeListener: Option[(Option[Throwable]) => Unit] = None,
+    reconnectListener: Option[() => Unit] = None,
+    errorResponseListener: Option[(PubSubErrorResponse) => Unit] = None,
+    rawRecordListener: Option[(String) => Unit] = None,
     url: String = "wss://api.cogswell.io/pubsub"
-)
+) {
+  /**
+   * Supplies a copy the same options with a different URL.
+   * 
+   * @param altUrl the alternate URL
+   * 
+   * @return the new options containing the alternate URL
+   */
+  def withUrl(altUrl: String): PubSubOptions = {
+    PubSubOptions(
+      messageListener,
+      errorListener,
+      closeListener,
+      reconnectListener,
+      errorResponseListener,
+      rawRecordListener,
+      altUrl
+    )
+  }
+
+  def messageHandler: Option[PubSubMessageHandler] = messageListener.map(CogsUtils.messageHandler(_))
+  def errorHandler: Option[PubSubErrorHandler] = errorListener.map(CogsUtils.errorHandler(_))
+  def closeHandler: Option[PubSubCloseHandler] = closeListener.map(CogsUtils.closeHandler(_))
+  def reconnectHandler: Option[PubSubReconnectHandler] = reconnectListener.map(CogsUtils.reconnectHandler(_))
+  def errorResponseHandler: Option[PubSubErrorResponseHandler] = errorResponseListener.map(CogsUtils.errorResponseHandler(_))
+  def rawRecordHandler: Option[PubSubRawRecordHandler] = rawRecordListener.map(CogsUtils.rawRecordHandler(_))
+}
 
 object PubSubOptions {
   /**
--- src/main/scala/io/cogswell/dslink/pubsub/services/CogsPubSubService.scala
+++ src/main/scala/io/cogswell/dslink/pubsub/services/CogsPubSubService.scala
@@ -11,12 +11,21 @@ import java.time.Duration
 import io.cogswell.dslink.pubsub.connection.CogsPubSubConnection
 import io.cogswell.dslink.pubsub.connection.PubSubConnection
 import io.cogswell.dslink.pubsub.model.PubSubOptions
+import com.gambit.sdk.pubsub.handlers.PubSubCloseHandler
+import com.gambit.sdk.pubsub.handlers.PubSubReconnectHandler
+import com.gambit.sdk.pubsub.PubSubMessageRecord
+import com.gambit.sdk.pubsub.handlers.PubSubMessageHandler
+import io.cogswell.dslink.pubsub.model.PubSubMessage
+import com.gambit.sdk.pubsub.handlers.PubSubErrorHandler
+import com.gambit.sdk.pubsub.responses.errors.PubSubErrorResponse
+import com.gambit.sdk.pubsub.handlers.PubSubErrorResponseHandler
+import com.gambit.sdk.pubsub.handlers.PubSubRawRecordHandler
 
 object CogsPubSubService extends PubSubService {
   private def translateOptions(options: PubSubOptions): CogsPubSubOptions = {
     new CogsPubSubOptions(options.url, true, Duration.ofSeconds(30), null)
   }
-
+  
   override def connect(
       keys: Seq[String],
       options: Option[PubSubOptions]
@@ -26,6 +35,16 @@ object CogsPubSubService extends PubSubService {
     Futures.convert(
         PubSubSDK.getInstance.connect(keys.toList.asJava, cogsOptions)
     ) map { handle =>
+      // Add handlers to the handle
+      options.foreach { opts =>
+        opts.closeHandler.foreach(handle.onClose(_))
+        opts.reconnectHandler.foreach(handle.onReconnect(_))
+        opts.messageHandler.foreach(handle.onMessage(_))
+        opts.errorHandler.foreach(handle.onError(_))
+        opts.errorResponseHandler.foreach(handle.onErrorResponse(_))
+        opts.rawRecordHandler.foreach(handle.onRawRecord(_))
+      }
+      
       CogsPubSubConnection(handle)
     }
   }
